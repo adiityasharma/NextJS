2189d21d13b9420f3a9c7629ac1a37877abadb00
Aditya Sharma <adityasharma626367@gmail.com>
2025-07-11 23:57:28 +0530

    feat: enhance dashboard and home page layout with improved styling and content

--- dashboard/src/app/dashboard/page.tsx
+++ dashboard/src/app/dashboard/page.tsx
@@ -1,9 +1,28 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Dashboard() {
   return (
-    <div>
-      Dashboard
+    <div className='min-h-screen p-8'>
+      <div className='max-w-6xl mx-auto bg-slate-800 rounded-2xl shadow-gray-700 shadow-lg p-8 space-y-8'>
+        <div className='flex flex-col'>
+          <h1 className='text-3xl font-extrabold text-gray-300 mb-4 sm:mb-0 '>Dashboard</h1>
+          <p className='text-md text-gray-400 mt-3'>
+            Welcome back! Here you can manage your tasks, view analytics, and make adjestments.
+          </p>
+        </div>
+        {/* Dashboard content */}
+        <div>
+        {/* task card */}
+          <Link href={""}>
+            <div>
+              <h2>Tasks</h2>
+              <span>📃</span>
+            </div>
+            <p>View, orgnize and manange your daily tasks.</p>
+          </Link>
+        </div>
+      </div>
     </div>
   )
 }
--- dashboard/src/app/page.tsx
+++ dashboard/src/app/page.tsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
 
 
 export default function Home() {
   return (
-    <>
-      This is Home
-   </>
+    <div  className="min-h-screen flex flex-col items-center justify-center p-6">
+      <div className="text-center">
+        <h1 className="text-5xl font-bold text-gray-400 mb-6" >Welcome to the Dashboard App</h1>
+        <p className="text-lg text-gray-500 mb-8">
+          Manage your tasks, track analytics, and stay organized with our powerful dashboard
+        </p>
+        <Link className="px-5 py-3 font-semibold bg-blue-500 rounded hover:bg-blue-600" href={"/dashboard"}>Go To the Dashboard</Link>
+      </div>
+    </div>
   );
 }
